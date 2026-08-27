b754fbfed515a4848bb25fb01435e9df44444432
CodeSync <codesync@users.noreply.github.com>
2026-08-27 13:45:59 +0530

    Solved: 0 (LeetCode) Sheets: NeetCode150, TopInterview150, Top100Liked, StriverA2Z, StriverSDE

---
+++ CodeSync/LeetCode/medium/linked-list/0/solution.c
@@ -0,0 +1,25 @@
+/*
+ * Platform: LeetCode
+ * Problem: 0
+ * URL: https://leetcode.com/problems/add-two-numbers/description/
+ * Language: C
+ * Difficulty: Medium
+ * Topics: Linked List, Math, Recursion
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-08-27T08:15:37.704Z
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
