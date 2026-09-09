735c4d816a0ede9a2f4e97377a2535ae167957ca
CodeSync <codesync@users.noreply.github.com>
2026-09-09 16:50:03 +0530

    Solved: 2 (LeetCode) Sheets: None

--- CodeSync/LeetCode/unknown/uncategorized/2/solution.c
+++ CodeSync/LeetCode/unknown/uncategorized/2/solution.c
@@ -1,32 +1,31 @@
 /*
  * Platform: LeetCode
  * Problem: 2
- * URL: https://leetcode.com/submissions/detail/2129184903/
+ * URL: https://leetcode.com/submissions/detail/2131815606/
  * Language: C
  * Difficulty: Unknown
  * Topics: Uncategorized
- * Runtime: 0 ms
- * Memory: 165.76 MB
- * Synced: 2026-09-09T11:18:38.631Z
+ * Runtime: 7 ms
+ * Memory: 202.56 MB
+ * Synced: 2026-09-09T11:20:03.158Z
  */
 
 1class Solution {
 2public:
-3    bool uniformArray(vector<int>& nums1) {
-4        int mo = INT_MAX;
-5        for(int i:nums1){
-6            if(i%2==1){
-7                mo=min(mo,i);
-8            }
+3    int firstStableIndex(vector<int>& nums, int k) {
+4        int n=nums.size();
+5        vector<int> revmin(n);
+6        revmin[n-1]=nums[n-1];
+7        for(int i=n-2;i>=0;--i){
+8            revmin[i]=min(nums[i],revmin[i+1]);
 9        }
-10        if(mo==INT_MAX){
-11            return true;
-12        }
-13        for(int i:nums1){
-14            if(i%2==0 && i<mo){
-15                return false;
-16            }
-17        }
-18        return true;
-19    }
-20};
+10        int start=nums[0];
+11        for(int i=0;i<n;i++){
+12            start=max(start,nums[i]);
+13            if(start-revmin[i]<=k){
+14                return i;
+15            }
+16        }
+17        return -1;
+18    }
+19};
